b5fa48d146867f9c9526f10e0bd7786da31f1c68
Adam Lord <adam@evergrow.com>
2025-09-17 13:31:38 -0700

    cute ingredients

--- frontend/recipe-robot/src/App.tsx
+++ frontend/recipe-robot/src/App.tsx
@@ -6,20 +6,20 @@ function App() {
     setIngredients([...ingredients, ingredient]);
   };
   function badgeVariant(i: number) {
-    const variants = ['primary', 'secondary', 'accent', 'neutral', 'info', 'success', 'warning', 'error'];
+    const variants = ['primary',  'accent', 'neutral', 'info', 'success', 'warning', 'error'];
     return variants[i % variants.length];
   }
   return (
-    <div className="min-h-screen hero">
-      <div className="text-center min-w-4xl bg-slate-700 p-6 rounded-xl space-y-6">
-        <h1 className=" text-5xl font-bold text-primary font-pressstart">
+    <div className="min-h-screen font-pressstart">
+      <div className="p-8 mx-auto mt-16 space-y-6 max-w-4xl text-center rounded-xl bg-slate-700">
+        <h1 className="text-5xl font-bold text-primary font-pressstart">
           Recipe Robot
         </h1>
-        <img src={robotImg} className="block mx-auto rounded-xl max-w-64" />
+        <img src={robotImg} className="block mx-auto max-w-64" />
         <div className="space-x-4">
           <input
-            className="input input-primary w-full input-xl"
-              placeholder="What you got on hand?"
+            className="w-full input input-primary input-xl"
+              placeholder={ingredients.length > 2 ? "Anything else?" : "What you got on hand?"}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   onAddIngredient(e.currentTarget.value);
@@ -28,11 +28,14 @@ function App() {
               }}
           />
         </div>
+        {ingredients.length > 0 && (
         <div className="flex flex-wrap gap-2 justify-center">
           {ingredients.map((ingredient, i) => (
             <div key={ingredient} className={`badge badge-${badgeVariant(i)}`}>{ingredient}</div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+        {ingredients.length > 2 && <button className="rounded-full animate-pulse btn btn-secondary btn-xl text-primary font-pressstart">Send It</button>}
       </div>
     </div>
   );
