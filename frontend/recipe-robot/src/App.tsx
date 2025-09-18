28a6a3ba498b5270dfc0bd9484c3eb15bd7e61e5
Adam Lord <adam@evergrow.com>
2025-09-17 18:18:21 -0700

    handle tags and stub submit

--- frontend/recipe-robot/src/App.tsx
+++ frontend/recipe-robot/src/App.tsx
@@ -5,10 +5,16 @@ function App() {
   const onAddIngredient = (ingredient: string) => {
     setIngredients([...ingredients, ingredient]);
   };
-  function badgeVariant(i: number) {
+  const badgeVariant = (i: number) => {
     const variants = ['primary',  'accent', 'neutral', 'info', 'success', 'warning', 'error'];
     return variants[i % variants.length];
   }
+  const removeIngredient = (i: number) => {
+    setIngredients(ingredients.filter((_, index) => index !== i));
+  };
+  const submitIngredients = () => {
+    alert("oh shit SUBMIT");
+  };
   return (
     <div className="min-h-screen font-pressstart">
       <div className="p-8 mx-auto mt-16 space-y-6 max-w-4xl text-center rounded-xl bg-slate-700">
@@ -17,25 +23,32 @@ function App() {
         </h1>
         <img src={robotImg} className="block mx-auto max-w-64" />
         <div className="space-x-4">
+        <label className="w-full rounded-xl join input input-xl input-primary">
           <input
-            className="w-full input input-primary input-xl"
               placeholder={ingredients.length > 2 ? "Anything else?" : "What you got on hand?"}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
-                  onAddIngredient(e.currentTarget.value);
-                  e.currentTarget.value = "";
+                  if(e.currentTarget.value.trim(). length > 0) {
+                    onAddIngredient(e.currentTarget.value);
+                    e.currentTarget.value = "";
+                  }
+                  if(e.shiftKey) {
+                   submitIngredients();
+                  }
                 }
               }}
           />
+          <button onClick={() => setIngredients([])} className="text-xl cursor-pointer">&times;</button>
+          </label>
         </div>
         {ingredients.length > 0 && (
         <div className="flex flex-wrap gap-2 justify-center">
           {ingredients.map((ingredient, i) => (
-            <div key={ingredient} className={`badge badge-${badgeVariant(i)}`}>{ingredient}</div>
+            <div key={ingredient} className={`items-center badge badge-${badgeVariant(i)}`}><span>{ingredient}</span><span className="opacity-50 cursor-pointer hover:opacity-100" onClick={() => removeIngredient(i)}>x</span></div>
             ))}
           </div>
         )}
-        {ingredients.length > 2 && <button className="rounded-full animate-pulse btn btn-secondary btn-xl text-primary font-pressstart">Send It</button>}
+        {ingredients.length > 2 && <button className="rounded-full animate-pulse btn btn-secondary btn-xl text-primary font-pressstart" onClick={submitIngredients}>Send It</button>}
       </div>
     </div>
   );
